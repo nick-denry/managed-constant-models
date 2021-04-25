090a0677552ec17bc1245a84ac602f2ef139f60e
Nick Denry <nick.denry@gmail.com>
2021-04-26 00:58:31 +0300

    Swap trait with iface

--- src/interfaces/ManagedConstantInterface.php
+++ src/interfaces/ManagedConstantInterface.php
@@ -1,71 +1,47 @@
 <?php
 
-/**
- * Trait to get managed constants data.
- */
+namespace nickdenry\managedConstants\interfaces;
 
-namespace nickdenry\managedConstants\traits;
-
-trait ManagedConstantTrait
+interface ManagedConstantInterface
 {
 
-    private static $attributes = '_ATTRIBUTES';
+    /**
+     * Return array off the class constants, except the _ATTRIBUTES one
+     * ['ACTIVE' => 0, 'DONE' => 1]
+     * @return array
+     */
+    public static function constants();
 
-    public static function constants()
-    {
+    /**
+     * Get class constant values
+     * [0, 1]
+     * @return array
+     */
+    public static function values();
 
-        $reflection = new \ReflectionClass(static::class);
-        $allConstants = $reflection->getConstants();
+    /**
+     * Get list of the constant attributes
+     * ['label' => 'Constant related label', 'class' => 'Constant related class']
+     * @param int|string $constValue constant value
+     * @return array Constant value _ATTRIBUTES entry
+     * @throws \Exception
+     */
+    public static function listAttributes($constValue);
 
-        $constants = [];
-        foreach ($allConstants as $name => $value) {
-            if ($name === self::$attributes) {
-                continue;
-            }
-            $constants[$name] = $value;
-        }
-        return $constants;
-    }
+    /**
+     * Get constant attribute by it's name
+     * i.e. value 'Constant related label'
+     * @param int|string $constValue constant value
+     * @param string $attributeName _ATTRIBUTES string attribute
+     * @return mixed
+     * @throws \Exception
+     */
+    public static function attribute($constValue, $attributeName);
 
-    public static function values()
-    {
-        return array_values(static::constants());
-    }
-
-    public static function listAttributes($constValue)
-    {
-        try {
-            $descriptionArray = constant('static::' . self::$attributes);
-        } catch (\Exception $ex) {
-            throw new \Exception('const array _ATTRIBUTES is not set at the ' . static::class);
-        }
-        if (!array_key_exists($constValue, $descriptionArray)) {
-            $constants = array_flip(self::getConstants());
-            throw new \Exception('_ATTRIBUTE description for the const ' . $constants[$constValue] . ' is not set at the ' . static::class);
-        }
-
-        return $descriptionArray[$constValue];
-    }
-
-    public static function attribute($constValue, $attributeName)
-    {
-        $valueAttributes = self::listAttributes($constValue);
-        if (!array_key_exists($attributeName, $valueAttributes)) {
-            $constants = array_flip(self::getConstants());
-            throw new \Exception('There is no _ATTRIBUTE enrty "' . $attributeName . '" for the const ' . $constants[$constValue] . ' at the ' . static::class);
-        }
-        return $valueAttributes[$attributeName];
-    }
-
-    public function getList()
-    {
-        $constValues = static::values();
-        $constList = [];
-        foreach ($constValues as $value) {
-            $constList[] = array_merge(['id' => $value], static::listAttributes($value));
-        }
-        return $constList;
-    }
-
+    /**
+     * Return array of the class constant with atributes like
+     * ['id' => 0, 'label' => 'Some attribute label', 'class' => 'Constant related class']
+     * @return array
+     */
+    public function getList();
 }
-
